b9fbe3590d03f1a02f7a9b14b53ea45e8ba0d4ff
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-04-05 04:07:42 +0300

    Solved set union operation

---
+++ set_union.py
@@ -0,0 +1,6 @@
+eng_subs_no = int(input())
+eng_subs = set(map(int, input().split()))
+french_subs_no = int(input())
+french_subs = set(map(int, input().split()))
+
+print(len(eng_subs.union(french_subs)))
