5a3293df157f42422eb59e55551af570bf76c7fc
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-05-01 11:32:27 +0300

    Solved score_of_parenthesis

---
+++ score_of_parenthesis.py
@@ -0,0 +1,28 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        scores = []
+        stack = []
+        init_score = 1/ 2
+        score = init_score
+        
+        for i in range(len(s)):
+            brace = s[i]
+            if brace == '(':
+                if len(stack) >= 1 and score != 1 / 2:
+                    scores.append(int(score * 2**len(stack)))
+                elif score != 1 / 2:
+                    scores.append(int(score))
+                score = init_score
+                stack.append(brace)
+            else:
+                if stack:
+                    stack.pop()
+                    score *= 2
+            if i == len(s) - 1:
+                scores.append(int(score))
+                score = init_score
+
+                
+
+        return sum(scores)
+            
