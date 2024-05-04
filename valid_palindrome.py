aad80016624c0f46321cd92de638738c2079a42d
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-05-04 13:21:41 +0300

    Solved valid palidrome

---
+++ valid_palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l].isalnum() and s[r].isalnum():
+                if s[l].lower == s[r].lower:
+                    l += 1
+                    r -= 1
+                else:
+                    print(s[l])
+                    print(s[r])
+                    return False
+            elif s[l].isalnum():
+                r -= 1
+            else:
+                l += 1
+
+        return True
+    
