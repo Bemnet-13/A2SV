0f6d932b4afcac5c414a8e111e074db1d937fd1e
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-07-27 16:20:15 +0300

    Solved minimum card pickup

---
+++ minimum_consecutive_cards.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        card_stack = Counter()
+        min_cards = len(cards) + 1
+
+        for i in range(len(cards)):
+            if cards[i] in card_stack:
+                min_cards = min(min_cards, i - card_stack[cards[i]] + 1)  
+            card_stack[cards[i]] = i
+        
+        if min_cards == len(cards) + 1:
+            return -1
+        return min_cards
