98f1c77f6e596d98c9da64655b68c3abc0a73b39
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-02-12 10:41:05 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 12-Feb-2025/Merging Arrays 258931.py
@@ -0,0 +1,21 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n, m = map(int, input().split())
+
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+ans = []
+
+i = j = 0
+
+while i < n or j < m:
+    if j == m or i < n and arr1[i] < arr2[j]:
+        ans.append(arr1[i])
+        i += 1
+    else:
+        ans.append(arr2[j])
+        j += 1
+
+# ans.extend(arr1[i:] + arr2[j:])
+print(*ans)
