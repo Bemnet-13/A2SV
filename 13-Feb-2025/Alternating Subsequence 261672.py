1f21254353b9cf20e011dd52c62b05edcd536407
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-02-13 03:52:43 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 13-Feb-2025/Alternating Subsequence 261672.py
@@ -0,0 +1,21 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    curr_max = a[0]
+    max_sum = 0
+
+    for right in range(1, n):
+        if a[right] * a[right - 1] > 0:
+            curr_max = max(curr_max, a[right])
+        else:
+            max_sum += curr_max
+            curr_max = a[right]
+
+
+
+    print(max_sum + curr_max)
