0b2ee51409e57457734effccc853b0b94543cb01
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-02-21 21:34:01 +0300

    Created 'Circular Array Loop' by A2SV Hub

---
+++ 21-Feb-2025/Circular Array Loop 279429.py
@@ -0,0 +1,25 @@
+# Problem: Circular Array Loop - https://leetcode.com/problems/circular-array-loop/description/
+
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n):
+            starting = i
+            curr = starting
+            k = 0
+            seen = set([starting]) 
+            while k < n:
+                next_ = (curr + nums[curr]) % n
+                if not self.sameSign(nums[starting], nums[next_]):
+                    break
+                k += 1
+                curr = next_
+                seen.add(curr)
+                if k > 1 and starting == curr and len(seen) >= 2:
+                    return True
+        return False
+    
+    def sameSign(self, a, b):
+        if (a > 0 and b > 0) or (a < 0 and b < 0):
+            return True
+        return False
