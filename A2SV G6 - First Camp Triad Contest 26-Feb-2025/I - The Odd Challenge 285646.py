33c41043a7162e56ef89c94c15f5e8462aa978da
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-02-27 02:38:54 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 26-Feb-2025/I - The Odd Challenge 285646.py
@@ -0,0 +1,39 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+import sys
+from itertools import accumulate
+
+def input():
+    return sys.stdin.readline().strip()
+
+def int_input():
+    return int(input())
+
+def list_input():
+    return list(map(int, input().split()))
+
+def solve():
+    # Your solution logic here
+    n, q = map(int, input().split())
+    arr = [0] + list_input()
+    ps = list(accumulate(arr))
+
+
+    for _ in range(q):
+        l, r, k = map(int, input().split())
+        added = (r - l + 1) * k
+        removed = ps[r] - ps[l - 1]
+        ans = ps[-1] + added - removed
+        if ans % 2 == 1:
+            print("YES")
+        else:
+            print("NO")
+
+def main():
+    t = int_input()  # Number of test cases
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
+    
