de219e859905028434883d8229ff1b8518efc575
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-03-04 16:33:42 +0300

    Created 'Lemonade Change' by A2SV Hub

---
+++ 04-Mar-2025/Lemonade Change 291113.py
@@ -0,0 +1,22 @@
+# Problem: Lemonade Change - https://leetcode.com/problems/lemonade-change/
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = tens = 0
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+                continue
+            if bill == 10:
+                tens += 1
+            else:
+                if tens > 0:
+                    tens -= 1
+                else:
+                    fives -= 2
+            fives -= 1
+        
+            if fives < 0:
+                return False
+        
+        return True
