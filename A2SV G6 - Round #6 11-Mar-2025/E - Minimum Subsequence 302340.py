6e739eca49cfc037f576e11b40397a6a7ce0f456
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-03-11 14:46:42 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/E - Minimum Subsequence 302340.py
@@ -0,0 +1,57 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+import sys
+
+def input():
+    return sys.stdin.readline().strip()
+
+def int_map():
+    return map(int, input().split())
+
+def int_input():
+    return int(input())
+
+def list_input():
+    return list(map(int, input().split()))
+
+def solve():
+    # Your solution logic here
+    n = int_input()
+    num = input()
+
+
+    count = 0
+    zeros = []
+    ones = []
+    ans = [1] * n
+
+    for i in range(n):
+        if num[i] == '1':
+            if zeros:
+                c = zeros.pop()
+            else:
+                count += 1
+                c = count
+            ans[i] = c
+            ones.append(c)
+        else:
+            if ones:
+                d = ones.pop()
+            else:
+                count += 1
+                d = count
+            ans[i] = d
+            zeros.append(d)
+
+    print(len(ones) + len(zeros))
+    print(*ans)
+
+        
+
+def main():
+    t = int_input()  # Number of test cases
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
