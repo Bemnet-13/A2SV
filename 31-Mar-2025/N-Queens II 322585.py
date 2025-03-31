a9f7877fcca752f3800d76e802077b437e20d9b8
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-03-31 16:46:54 +0300

    Created 'N-Queens II' by A2SV Hub

---
+++ 31-Mar-2025/N-Queens II 322585.py
@@ -0,0 +1,34 @@
+# Problem: N-Queens II - https://leetcode.com/problems/n-queens-ii/description/
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ans = 0
+
+        def isClear(x, y, dirs, prev):
+            while 0 <= x < n and 0 <= y < n:
+                x = x + dirs[0]
+                y = y + dirs[1]
+                if (x, y) in prev:
+                    return False
+            return True
+        
+        def backtrack(path, seen, prev):
+            nonlocal ans
+            if len(path) == n:
+                ans += 1
+                return
+            
+            for i in range(n):
+                if i not in prev:
+                    if isClear(i, len(seen), [-1, -1], seen) and \
+                     isClear(i, len(seen), [1, -1], seen):
+                        path.append(i)
+                        prev.add(i)
+                        seen.add((i, len(path) - 1))
+                        backtrack(path, seen, prev)
+                        path.pop()
+                        prev.remove(i)
+                        seen.remove((i, len(path)))
+                        
+        backtrack([], set(), set())
+        return ans
