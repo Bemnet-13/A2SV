00a0782268df7149a02ccf6d8f3fe2604b6516f7
Bemnet Aschalew <147504132+Bemnet-13@users.noreply.github.com>
2025-04-22 17:40:45 +0300

    Created 'Belted Rooms' by A2SV Hub

---
+++ 22-Apr-2025/Belted Rooms 338609.py
@@ -0,0 +1,27 @@
+# Problem: Belted Rooms - https://codeforces.com/problemset/problem/1428/B
+
+from collections import defaultdict
+
+
+t = int(input())
+for _ in range(t):
+
+    n = int(input())
+    s = input()
+
+    # Count the number of Clockwise and Anti-clockwise belts
+    count_clock = count_anti = 0
+    for i in range(n):
+        if s[i] == '>':
+            count_clock += 1
+        elif s[i] == '<':
+            count_anti += 1
+    count = 0
+    if count_anti and count_clock:
+        for i in range(n):
+            if s[i] == '-' or s[(i + 1) % n] == '-':
+                count += 1
+    else:
+        count = n
+    
+    print(count)
